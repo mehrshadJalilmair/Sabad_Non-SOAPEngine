407d93288113d585c6e829e521dcf7c6e1d6f36f
mehrshadJalilmair <mehrshadjalilmasir@gmail.com>
2017-02-24 14:48:42 +0330

    Last
    Last

--- Sabad/StoreCreation.swift
+++ Sabad/StoreCreation.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class StoreCreation: UIViewController {
+class StoreCreation: UIViewController , UIScrollViewDelegate{
 
+    @IBOutlet var scrollView: UIScrollView!
     
     let ImagesContainer : UIView! = {
         
@@ -18,6 +19,7 @@ class StoreCreation: UIViewController {
         filterView.translatesAutoresizingMaskIntoConstraints = false
         filterView.layer.cornerRadius = 3
         filterView.layer.masksToBounds = true
+        filterView.backgroundColor = UIColor.red
         return filterView
     }()
     
@@ -28,6 +30,7 @@ class StoreCreation: UIViewController {
         filterView.translatesAutoresizingMaskIntoConstraints = false
         filterView.layer.cornerRadius = 3
         filterView.layer.masksToBounds = true
+        filterView.backgroundColor = UIColor.red
         return filterView
     }()
     
@@ -38,6 +41,7 @@ class StoreCreation: UIViewController {
         filterView.translatesAutoresizingMaskIntoConstraints = false
         filterView.layer.cornerRadius = 3
         filterView.layer.masksToBounds = true
+        filterView.backgroundColor = UIColor.red
         return filterView
     }()
     
@@ -48,6 +52,7 @@ class StoreCreation: UIViewController {
         filterView.translatesAutoresizingMaskIntoConstraints = false
         filterView.layer.cornerRadius = 3
         filterView.layer.masksToBounds = true
+        filterView.backgroundColor = UIColor.red
         return filterView
     }()
     
@@ -58,6 +63,7 @@ class StoreCreation: UIViewController {
         filterView.translatesAutoresizingMaskIntoConstraints = false
         filterView.layer.cornerRadius = 3
         filterView.layer.masksToBounds = true
+        filterView.backgroundColor = UIColor.red
         return filterView
     }()
     
@@ -68,6 +74,7 @@ class StoreCreation: UIViewController {
         filterView.translatesAutoresizingMaskIntoConstraints = false
         filterView.layer.cornerRadius = 3
         filterView.layer.masksToBounds = true
+        filterView.backgroundColor = UIColor.red
         return filterView
     }()
     
@@ -75,8 +82,122 @@ class StoreCreation: UIViewController {
         super.viewDidLoad()
 
         openGettingStoreFields = false
-        //print(storeTwon)
-        //print(storeMall)
+        initImagesContainer()
+        initNameContainer()
+        initManagementContainer()
+        initPhoneContainer()
+        initDescriptionContainer()
+        initAddressContainer()
+    }
+
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        setScrollViewContentSize()
+    }
+    
+    func setScrollViewContentSize() {
+        
+        var height: CGFloat
+        
+        let lastViewYPos = self.AddressContainer.frame.origin.y  // this is absolute positioning, not relative
+        let lastViewHeight = self.AddressContainer.frame.size.height
+        
+        height = lastViewYPos + lastViewHeight + 5
+    
+        scrollView.contentSize.height = height
+        
+        scrollView.isUserInteractionEnabled = true
+        scrollView.isScrollEnabled = true
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+    }
+    
+    func initImagesContainer()
+    {
+        //let width = ((SCREEN_SIZE.width) / 2) - 10
+        let height = ((SCREEN_SIZE.width) / 2)
+        
+        scrollView.addSubview(ImagesContainer)
+        //x
+        let horizontalConstraint = NSLayoutConstraint(item: ImagesContainer, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+        //y
+        let verticalConstraint = NSLayoutConstraint(item: ImagesContainer, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.top, multiplier: 1, constant: +5)
+        //w
+        let widthConstraint = NSLayoutConstraint(item: ImagesContainer, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.width, multiplier: 1, constant: -10)
+        //h
+        let heightConstraint = NSLayoutConstraint(item: ImagesContainer, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: height)
+        NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+    }
+    
+    func initNameContainer()
+    {
+        scrollView.addSubview(NameContainer)
+        //x
+        let horizontalConstraint = NSLayoutConstraint(item: NameContainer, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: ImagesContainer, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: +5)
+        //y
+        let verticalConstraint = NSLayoutConstraint(item: NameContainer, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+        //w
+        let widthConstraint = NSLayoutConstraint(item: NameContainer, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.width, multiplier: 1, constant: -10)
+        //h
+        let heightConstraint = NSLayoutConstraint(item: NameContainer, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.height, multiplier: 1/6, constant: 0)
+        NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+    }
+    
+    func initManagementContainer()
+    {
+        scrollView.addSubview(ManagementContainer)
+        //x
+        let horizontalConstraint = NSLayoutConstraint(item: ManagementContainer, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: NameContainer, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: +5)
+        //y
+        let verticalConstraint = NSLayoutConstraint(item: ManagementContainer, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+        //w
+        let widthConstraint = NSLayoutConstraint(item: ManagementContainer, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.width, multiplier: 1, constant: -10)
+        //h
+        let heightConstraint = NSLayoutConstraint(item: ManagementContainer, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.height, multiplier: 1/6, constant: 0)
+        NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+    }
+    
+    func initPhoneContainer()
+    {
+        scrollView.addSubview(PhoneContainer)
+        //x
+        let horizontalConstraint = NSLayoutConstraint(item: PhoneContainer, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: ManagementContainer, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: +5)
+        //y
+        let verticalConstraint = NSLayoutConstraint(item: PhoneContainer, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+        //w
+        let widthConstraint = NSLayoutConstraint(item: PhoneContainer, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.width, multiplier: 1, constant: -10)
+        //h
+        let heightConstraint = NSLayoutConstraint(item: PhoneContainer, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.height, multiplier: 1/6, constant: 0)
+        NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+    }
+    
+    func initDescriptionContainer()
+    {
+        scrollView.addSubview(DescriptionContainer)
+        //x
+        let horizontalConstraint = NSLayoutConstraint(item: DescriptionContainer, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: PhoneContainer, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: +5)
+        //y
+        let verticalConstraint = NSLayoutConstraint(item: DescriptionContainer, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+        //w
+        let widthConstraint = NSLayoutConstraint(item: DescriptionContainer, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.width, multiplier: 1, constant: -10)
+        //h
+        let heightConstraint = NSLayoutConstraint(item: DescriptionContainer, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.height, multiplier: 1/6, constant: 0)
+        NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+    }
+    
+    func initAddressContainer()
+    {
+        scrollView.addSubview(AddressContainer)
+        //x
+        let horizontalConstraint = NSLayoutConstraint(item: AddressContainer, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: DescriptionContainer, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: +5)
+        //y
+        let verticalConstraint = NSLayoutConstraint(item: AddressContainer, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+        //w
+        let widthConstraint = NSLayoutConstraint(item: AddressContainer, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.width, multiplier: 1, constant: -10)
+        //h
+        let heightConstraint = NSLayoutConstraint(item: AddressContainer, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: scrollView, attribute: NSLayoutAttribute.height, multiplier: 1/6, constant: 0)
+        NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
     }
 }
 
